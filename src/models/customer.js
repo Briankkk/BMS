47f129b922022b85d6a7c2959208d81a2b9f0753
brian <13813009693@139.com>
2018-05-31 16:23:33 +0800

    init

---
+++ src/models/customer.js
@@ -0,0 +1,21 @@
+export default {
+  namespace: 'customer',
+
+  state: {
+    data: {
+      list: [],
+      pagination: {},
+    },
+  },
+
+  effects: {},
+
+  reducers: {
+    save(state, action) {
+      return {
+        ...state,
+        data: action.payload,
+      };
+    },
+  },
+};
